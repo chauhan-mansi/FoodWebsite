a84465617f9a2365846e86673ca6fd5241d973fb
Mansi Chauhan <chauhanmansi2823@gmail.com>
2024-05-26 11:27:47 +0530

    added a databse file

--- backend/app.js
+++ backend/app.js
@@ -6,17 +6,14 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-app.use(cors({
-    origin: [],
-    methods: [],
-    credentials:
-}))
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        methods: ["POST"],
+        credentials: true
+    }));
 
-
-
-
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
